00ef3d8ec409f6e7c33bbd3f828ecb68cfbdd05a
Guapura89 <betocrack1228@gmail.com>
2023-03-15 20:42:50 -0600

    feat: add post method :sparkles:

--- src/utils/main.js
+++ src/utils/main.js
@@ -1,4 +1,6 @@
-API_URL = "https://api.escuelajs.co/api/v1/products?limit=18&offset=1";
+//import fetch from "node-fetch";
+
+API_URL = "https://api.escuelajs.co/api/v1/users";
 const app = document.getElementById("app");
 
 const getData = async (url_api) => {
@@ -14,12 +16,45 @@ const getData = async (url_api) => {
   }
 };
 
+// post user
+const postData = async (url_api) => {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const avatar = "https://cdn-icons-png.flaticon.com/128/2202/2202112.png";
+  const datas = {
+    email,
+    name,
+    password,
+    role: "customer",
+    avatar,
+  };
+  try {
+    const response = await fetch(url_api, {
+      method: "post",
+      body: JSON.stringify(datas),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+function createUser() {
+  postData(API_URL).then((res) => {
+    console.log(res);
+    window.location.reload();
+  });
+}
+
 getData(API_URL).then((data) => {
   data.map((item) => {
     app.innerHTML += `
         <div class="p-3 bg-slate-300 hover:bg-slate-500 transition rounded-lg flex flex-col hover:text-white justify-center text-center hover:cursor-pointer hover:scale-105">
-            <h2 class="font-bold my-3">${item.title}</h2>
-            <img class="rounded-lg" src="${item.images[0]}" alt="${item.title}">
+            <h2 class="font-bold my-3">${item.name}</h2>
+            <img class="rounded-lg" src="${item.avatar}" alt="${item.name}">
         </div>`;
   });
 });
